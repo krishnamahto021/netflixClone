2b523884a2af65544c9c027174c0c5366a682d05
krishna mahto <mahtok98212@gmail.com>
2023-11-22 12:03:07 +0530

    Add the basic configuration for the production

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
 const passport = require("passport");
+const path = require("path");
 const passportJWT = require("./config/passport-jwt-strategy");
 
 const app = express();
@@ -13,6 +14,21 @@ connectDB();
 app.use(passport.initialize());
 
 app.use("/", require("./routes"));
+
+/*---------------------DEPLOYMENT-----------------------*/
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+}else{
+  app.get("/",(req,res)=>{
+    res.send("Running on development")
+  })
+}
+
+/*---------------------DEPLOYMENT-----------------------*/
 
 const PORT = process.env.PORT || 5000;
 
